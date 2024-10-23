602e7d4660a8100f33d470bf5fbc43458295d475
Rick <rick@pop-os.tailba65e2.ts.net>
2024-10-22 22:42:35 -0300

    last-commit

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\PomodoroController;
 Route::prefix('pomos')->group(function () {
     Route::post('/send', [PomodoroController::class, 'store'])->name('store-pom');
     Route::get('/pomos-list', [PomodoroController::class, 'show'])->name('pomos-list');
-    Route::get('/pomos-search/{id}', [PomodoroController::class, 'show'])->name('pomos-list');
+    Route::get('/pomos-search/{id}', [PomodoroController::class, 'search'])->name('pomos-list');
     Route::put('/pomos-edit/{id}', [PomodoroController::class, 'update'])->name('pomos-edit');
     Route::delete('/pomos-delete/{id}', [PomodoroController::class, 'delete'])->name('pomos-delete');
 });
